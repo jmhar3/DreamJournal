3e5a510d3cefe853c8eabbc08301377de79a3f5f
Jessica Harriman <jmhar@protonmail.com>
2021-11-29 20:39:28 +1100

    create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,15 @@
 class UsersController < ApplicationController
-    skip_before_action :require_login, only: [:create]
+    skip_before_action :authorized, only: [:create]
+    wrap_parameters :user, include: [:name, :email, :password, :password_confirmation]
 
     def create
       user = User.create(user_params)
       user.image = params[:image]
-      if user.save
+      if user.valid?
         token = encode_token({user_id: user.id})
         render json: {token: token}, status: :created
       else
-        render json: user.errors, status: :unprocessable_entity
+        render json: user.errors, status: :not_acceptable
       end
     end
   
@@ -32,6 +33,6 @@ class UsersController < ApplicationController
     private
   
     def user_params
-      params.require(:user).permit(:username, :email, :password, :password_confirmation, :image, :image_cache)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cache)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :users
 
   post 'login', to: 'sessions#login'
+  post 'signup', to: 'users#create'
 
   get '/auth/facebook/callback' => 'sessions#fb_login'
 end
