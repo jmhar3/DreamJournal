5d9b2b83a5f26c0278378c8a8ddeab848352d527
Jessica Harriman <jmhar@protonmail.com>
2021-12-10 17:17:42 +1100

    add namespaces to routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  resources :transactions
-  resources :notes_categories
-  resources :categories
-  resources :notes
-  resources :goals
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      resources :transactions
+      resources :notes_categories
+      resources :categories
+      resources :notes
+      resources :goals
+      resources :users
 
-  post 'login', to: 'sessions#login'
-  post 'signup', to: 'users#create'
-  post 'logout', to: 'sessions#logout'
+      post 'login', to: 'sessions#login'
+      post 'signup', to: 'users#create'
+      post 'logout', to: 'sessions#logout'
 
-  get '/auth/facebook/callback' => 'sessions#fb_login'
+      get '/auth/facebook/callback' => 'sessions#fb_login'
+    end
+  end
 end
